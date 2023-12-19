522bc44922c44bea1f5ee9b255e0cb81b172ab49
Grzegorz <grzegorz1006.98@wp.pl>
2023-12-19 20:28:45 +0100

    Model for events Event

--- events/tests.py
+++ events/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
 
-# Create your tests here.
+from events.models import Event 
+
+
+class EventModelTest(TestCase):
+
+    def test_saving_events(self):
+
+        first_event = Event()
+        first_event.title = 'First event title'
+        first_event.save()
+
+        second_event = Event()
+        second_event.title = 'Second event title'
+        second_event.save()
+
+        saved_events = Event.objects.all()
+        self.assertEqual(saved_events.count(), 2)
+
+        first_event = saved_events[0]
+        second_event = saved_events[1]
+
+        self.assertEqual(first_event.title, 'First event title')
+        self.assertEqual(second_event.title, 'Second event title')  
+
+         
+
+
